fcd2a2b5f17065f0351e407eb15009fb481fba2e
johanzap <jfzapata318@misena.edu.co>
2017-08-23 09:03:07 -0500

    Se agregan algunas funcionalidades

--- Videoclub/client/app/login/login.controller.js
+++ Videoclub/client/app/login/login.controller.js
@@ -1,39 +1,49 @@
-'use strict';
+  'use strict';
 
-(function(){
+  (function(){
 
-class LoginComponent {
-  constructor(authService,$auth) {
-    this.authService = authService;
-    this.$auth = $auth;
-    this.error = "";
-    this.user = {
-      numDocumento:'',
-      password:''
-    }
-  }
-  login(){
-    this.$auth.login(this.user)
-    .then(response =>{
-      console.log(response);
-    })
-    .catch(err =>{
-      console.log(this.user);
-      if(this.user.numDocumento == '' && this.user.password == ''){
-        this.error = "Debe ingresar sus datos";
-      }else{
-        this.error = err.data;
+  class LoginComponent {
+    constructor(authService,$auth) {
+      this.authService = authService;
+      this.$auth = $auth;
+      this.error = "";
+      this.user = {
+        numDocumento:'',
+        password:''
       }
-      console.log(err);
-    })
-  }
-}
-LoginComponent.inject = ['authService'];
-angular.module('videoclubApp')
-  .component('login', {
-    templateUrl: 'app/login/login.html',
-    controller: LoginComponent,
-    controllerAs: 'vm'
-  });
+    }
+    login(){
+      this.$auth.login(this.user)
+      .then(response =>{
+        console.log(response);
+      })
+      .catch(err =>{
+        console.log(this.user);
+        if(this.user.numDocumento == '' && this.user.password == ''){
+          this.error = "Debe ingresar sus datos";
+        }else if(this.user.numDocumento == '' || this.user.password == ''){
+          if(this.user.numDocumento == ''){
+            this.error = 'Debe ingresar su numero de documento';
+          }
+          if(this.user.password == ''){
+            this.error = 'Debe ingresar su contraseña';
+          }
+        }else{
+          this.error = err.data;
+        }
+        console.log(err);
+      })
+    }
 
-})();
+
+  }
+
+  LoginComponent.inject = ['authService'];
+  angular.module('videoclubApp')
+    .component('login', {
+      templateUrl: 'app/login/login.html',
+      controller: LoginComponent,
+      controllerAs: 'vm'
+    });
+
+  })();
--- Videoclub/client/auth/auth.service.js
+++ Videoclub/client/auth/auth.service.js
@@ -1,71 +1,59 @@
-'use strict';
+	'use strict';
 
-function authService($auth,$state) {
-	var Auth = {
-    login: login,
-		isAuthenticated:isAuthenticated,
-		logout:logout,
-		isAdmin: isAdmin,
-		isUser: isUser
-  };
+	function authService($auth,$state) {
+		var Auth = {
+	    login: login,
+			isAuthenticated:isAuthenticated,
+			logout:logout,
+			isAdmin: isAdmin,
+			isUser: isUser
+	  };
 
-  function login(user){
-		$auth.login(user)
-		.then(response =>{
-			$state.go('main');
-			console.log('Login realizado correctamente');
-		})
-		.catch(err =>{
-			$state.go('login')
-			console.log('Error al loguearse');
-		})
-  }
-
-	function logout(){
-		if($auth.isAuthenticated()){ //$auth
-			$auth.logout()
+	  function login(user){
+			$auth.login(user)
 			.then(response =>{
 				$state.go('main');
-				console.log('Ha cerado sesion');
+				console.log('Login realizado correctamente');
 			})
-		}
-	}
+			.catch(err =>{
+				$state.go('login')
+				console.log('Error al loguearse');
+			})
+	  }
 
-	function isAuthenticated(){
-		if($auth.isAuthenticated()){ //isAuthenticated()
-			return true;
+		function logout(){
+			if($auth.isAuthenticated()){ //$auth
+				$auth.logout()
+				.then(response =>{
+					$state.go('main');
+					console.log('Ha cerado sesion');
+				})
+			}
+		}
+
+		function isAuthenticated(){
+			return $auth.isAuthenticated();
+		}
+
+		function isAdmin(){
+			if(Auth.isAuthenticated()){
+				return $auth.getPayload().roles.indexOf("ADMIN") !== -1;
 		}else{
 			return false;
 		}
 	}
 
-	function isAdmin(){
-		if(Auth.isAuthenticated()){
-			if($auth.getPayload().roles.indexOf("ADMIN") !== -1){
-				return true;
+	function isUser(){
+		if(Auth.isAuthenticated){
+			return $auth.getPayload().roles.indexOf("USER") !== -1;
 		}else{
 			return false;
 		}
-	}else{
-		return false;
 	}
-}
 
-function isUser(){
-	if(Auth.isAuthenticated){
-		if($auth.getPayload().roles.indexOf("USER") !== -1){
-			return true;
-	}else{
-		return false;
+
+	  return Auth;
 	}
-}else{
-	return false;
-}
-}
-
-
-  return Auth;
-}
-authService.inject = ['$auth','$state'];
-angular.module('videoclubApp')
-  .factory('authService', authService);
+	authService.inject = ['$auth','$state'];
+	angular.module('videoclubApp')
+	  .factory('authService', authService);
